d737a3cf1eeb8e5f74b00d35cd19e036723d0f92
Luca Spathmann <luca.spathmann@ato.de>
2020-01-31 12:31:46 +0100

    Addes NGX-SWIPER, Edited Style, Content

--- src/app/modules/startpage/startpage.module.ts
+++ src/app/modules/startpage/startpage.module.ts
@@ -4,6 +4,19 @@ import { CommonModule } from '@angular/common';
 import { StartpageRoutingModule } from './startpage-routing.module';
 import { StartpageStartComponent } from './pages/startpage-start/startpage-start.component';
 
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  keyboard: true,
+  mousewheel: true,
+  navigation: true,
+  pagination: false,
+  autoplay: true
+};
 
 @NgModule({
   declarations: [StartpageStartComponent],
@@ -12,7 +25,14 @@ import { StartpageStartComponent } from './pages/startpage-start/startpage-start
   ],
   imports: [
     CommonModule,
-    StartpageRoutingModule
+    StartpageRoutingModule,
+    SwiperModule
+  ],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
   ]
 })
 export class StartpageModule { }
