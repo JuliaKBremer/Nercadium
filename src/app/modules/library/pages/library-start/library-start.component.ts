24454f415c1c7658148028de358b7dc7c02f609f
Andust1234 <bosh@gmx.de>
2020-02-27 15:57:26 +0100

    added some dummy data, created a grid where they are shown, added a seperate grid where favorite assets are listed and added a search function

--- src/app/modules/library/pages/library-start/library-start.component.ts
+++ src/app/modules/library/pages/library-start/library-start.component.ts
@@ -7,6 +7,24 @@ import {MatGridListModule} from '@angular/material/grid-list';
   styleUrls: ['./library-start.component.css']
 })
 export class LibraryStartComponent implements OnInit {
+  searchtext = '';
+
+  objects = [
+    {text: 'Dungeon', cols: 1, rows: 2, tag: 'Scene', favorite: true},
+    {text: 'Owlbear', cols: 1, rows: 2, tag: 'Character', favorite: true},
+    {text: 'Dagger', cols: 1, rows: 2, tag: 'Object', favorite: false},
+    {text: 'Axe', cols: 1, rows: 2, tag: 'Object', favorite: true},
+    {text: 'Dangers in the big City', cols: 1, rows: 2, tag: 'Scripts', favorite: true},
+    {text: 'Healing Potion', cols: 1, rows: 2, tag: 'Object', favorite: false},
+    {text: 'obscure encounter', cols: 1, rows: 2, tag: 'Scripts', favorite: false},
+    {text: 'Sword', cols: 1, rows: 2, tag: 'Object', favorite: true},
+    {text: 'Club', cols: 1, rows: 2, tag: 'Object', favorite: true},
+    {text: 'Bow', cols: 1, rows: 2, tag: 'Object', favorite: false},
+    {text: 'Arrows', cols: 1, rows: 2, tag: 'Object', favorite: false},
+    {text: 'City', cols: 1, rows: 2, tag: 'Scene', favorite: true},
+    {text: 'Orc', cols: 1, rows: 2, tag: 'Character', favorite: false}
+  ];
+
 
   constructor() { }
 
