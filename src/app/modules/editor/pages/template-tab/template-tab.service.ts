93c7d46f3a21f45f3c745950c848648474a33fcc
Zombeicat92 <timo.mueller92@outlook.de>
2020-03-18 15:44:27 +0100

    #724602 Make fields positions relative and editable.

--- src/app/modules/editor/pages/template-tab/template-tab.service.ts
+++ src/app/modules/editor/pages/template-tab/template-tab.service.ts
@@ -137,9 +137,11 @@ export class TemplateTabService {
     const currentField: IField = currentTemplate.Fields.find(obj => obj.ID === fieldID);
 
     currentField.Properties.Type = {id: 1, value: fieldType, type: PropertyTypes.enum, enum: FieldTypes, checkChange: true};
+    currentField.Properties.Top = {id: 2, description: 'in px', value: 0, type: PropertyTypes.number};
+    currentField.Properties.Left = {id: 3, description: 'in px', value: 0, type: PropertyTypes.number};
 
     for (const key in currentField.Properties) {
-      if (key !== 'Name' && key !== 'Type') {
+      if (key !== 'Name' && key !== 'Type' && key !== 'Top' && key !== 'Left') {
         // noinspection JSUnfilteredForInLoop
         delete currentField.Properties[key];
       }
@@ -147,81 +149,81 @@ export class TemplateTabService {
 
     switch (fieldType) {
       case FieldTypes.TextBox: {
-        currentField.Properties.Label = {id: 2, value: 'TextBox', type: PropertyTypes.string};
-        currentField.Properties.MaxLength = {id: 3, value: 20, type: PropertyTypes.number};
-        currentField.Properties.Placeholder = {id: 5, value: 'Text Box', type: PropertyTypes.string};
-        currentField.Properties.ReadOnly = {id: 6, value: false, type: PropertyTypes.boolean};
-        currentField.Properties.Size = {id: 7, value: 20, type: PropertyTypes.number};
+        currentField.Properties.Label = {id: 4, value: 'TextBox', type: PropertyTypes.string};
+        currentField.Properties.MaxLength = {id: 5, value: 20, type: PropertyTypes.number};
+        currentField.Properties.Placeholder = {id: 6, value: 'Text Box', type: PropertyTypes.string};
+        currentField.Properties.ReadOnly = {id: 7, value: false, type: PropertyTypes.boolean};
+        currentField.Properties.Size = {id: 8, value: 20, type: PropertyTypes.number};
 
         currentTemplate.FieldValues[currentField.ID] = '';
         break;
       }
       case FieldTypes.TextArea: {
-        currentField.Properties.Label = {id: 2, value: 'TextArea', type: PropertyTypes.string};
-        currentField.Properties.Cols = {id: 3, value: 20, type: PropertyTypes.number};
-        currentField.Properties.Rows = {id: 4, value: 20, type: PropertyTypes.number};
-        currentField.Properties.MaxLength = {id: 5, value: 20, type: PropertyTypes.number};
-        currentField.Properties.Placeholder = {id: 7, value: 'Text Area', type: PropertyTypes.string};
-        currentField.Properties.ReadOnly = {id: 8, value: false, type: PropertyTypes.boolean};
-        currentField.Properties.Resize = {id: 9, value: false, type: PropertyTypes.boolean};
+        currentField.Properties.Label = {id: 4, value: 'TextArea', type: PropertyTypes.string};
+        currentField.Properties.Cols = {id: 5, value: 20, type: PropertyTypes.number};
+        currentField.Properties.Rows = {id: 6, value: 20, type: PropertyTypes.number};
+        currentField.Properties.MaxLength = {id: 7, value: 20, type: PropertyTypes.number};
+        currentField.Properties.Placeholder = {id: 8, value: 'Text Area', type: PropertyTypes.string};
+        currentField.Properties.ReadOnly = {id: 9, value: false, type: PropertyTypes.boolean};
+        currentField.Properties.Resize = {id: 10, value: false, type: PropertyTypes.boolean};
 
         currentTemplate.FieldValues[currentField.ID] = '';
         break;
       }
       case FieldTypes.Number: {
-        currentField.Properties.Label = {id: 2, value: 'Number', type: PropertyTypes.string};
-        currentField.Properties.Min = {id: 3, value: '0', type: PropertyTypes.number};
-        currentField.Properties.Max = {id: 4, value: '999', type: PropertyTypes.number};
-        currentField.Properties.Step = {id: 5, value: '1', type: PropertyTypes.number};
-        currentField.Properties.ReadOnly = {id: 6, value: false, type: PropertyTypes.boolean};
+        currentField.Properties.Label = {id: 4, value: 'Number', type: PropertyTypes.string};
+        currentField.Properties.Min = {id: 5, value: '0', type: PropertyTypes.number};
+        currentField.Properties.Max = {id: 6, value: '999', type: PropertyTypes.number};
+        currentField.Properties.Step = {id: 7, value: '1', type: PropertyTypes.number};
+        currentField.Properties.ReadOnly = {id: 8, value: false, type: PropertyTypes.boolean};
 
         currentTemplate.FieldValues[currentField.ID] = 0;
         break;
       }
       case FieldTypes.Select: {
-        currentField.Properties.Label = {id: 2, value: 'Select', type: PropertyTypes.string};
-        currentField.Properties.Options = {id: 3, value: ['Option'], type: PropertyTypes.options};
+        currentField.Properties.Label = {id: 4, value: 'Select', type: PropertyTypes.string};
+        currentField.Properties.Options = {id: 5, value: ['Option'], type: PropertyTypes.options};
 
         currentTemplate.FieldValues[currentField.ID] = 'Option';
         break;
       }
       case FieldTypes.CheckBox: {
-        currentField.Properties.Label = {id: 2, value: 'CheckBox', type: PropertyTypes.string};
+        currentField.Properties.Label = {id: 4, value: 'CheckBox', type: PropertyTypes.string};
 
         currentTemplate.FieldValues[currentField.ID] = false;
         break;
       }
       case FieldTypes.Radio: {
-        currentField.Properties.Label = {id: 2, value: 'Radio', type: PropertyTypes.string};
+        currentField.Properties.Label = {id: 4, value: 'Radio', type: PropertyTypes.string};
 
         currentTemplate.FieldValues[currentField.ID] = false;
         break;
       }
       case FieldTypes.Range: {
-        currentField.Properties.Label = {id: 2, value: 'Range', type: PropertyTypes.string};
-        currentField.Properties.Min = {id: 3, value: '0', type: PropertyTypes.number};
-        currentField.Properties.Max = {id: 4, value: '999', type: PropertyTypes.number};
-        currentField.Properties.Step = {id: 5, value: '1', type: PropertyTypes.number};
-        currentField.Properties.ReadOnly = {id: 6, value: false, type: PropertyTypes.boolean};
+        currentField.Properties.Label = {id: 4, value: 'Range', type: PropertyTypes.string};
+        currentField.Properties.Min = {id: 5, value: '0', type: PropertyTypes.number};
+        currentField.Properties.Max = {id: 6, value: '999', type: PropertyTypes.number};
+        currentField.Properties.Step = {id: 7, value: '1', type: PropertyTypes.number};
+        currentField.Properties.ReadOnly = {id: 8, value: false, type: PropertyTypes.boolean};
 
         currentTemplate.FieldValues[currentField.ID] = 0;
         break;
       }
       case FieldTypes.Table: {
-        currentField.Properties.Label = {id: 2, value: 'Table', type: PropertyTypes.string};
-        currentField.Properties.Rows = {id: 3, value: 10, type: PropertyTypes.number};
-        currentField.Properties.Cols = {id: 4, value: 10, type: PropertyTypes.number};
-        currentField.Properties.Width = {id: 5, description: 'in % oder px', value: '100%', type: PropertyTypes.string};
-        currentField.Properties.TableStyle = {id: 6, value: TableStyles.StyleOne,
+        currentField.Properties.Label = {id: 4, value: 'Table', type: PropertyTypes.string};
+        currentField.Properties.Rows = {id: 5, value: 10, type: PropertyTypes.number};
+        currentField.Properties.Cols = {id: 6, value: 10, type: PropertyTypes.number};
+        currentField.Properties.Width = {id: 6, description: 'in % oder px', value: '100%', type: PropertyTypes.string};
+        currentField.Properties.TableStyle = {id: 7, value: TableStyles.StyleOne,
           type: PropertyTypes.enum, enum: TableStyles, checkChange: false};
 
         currentTemplate.FieldValues[currentField.ID] = [];
         break;
       }
       case FieldTypes.Image: {
-        currentField.Properties.Label = {id: 2, value: 'Table', type: PropertyTypes.string};
-        currentField.Properties.Width = {id: 3, description: 'in % oder px', value: '100%', type: PropertyTypes.string};
-        currentField.Properties.Height = {id: 3, description: 'in % oder px', value: '', type: PropertyTypes.string};
+        currentField.Properties.Label = {id: 4, value: 'Table', type: PropertyTypes.string};
+        currentField.Properties.Width = {id: 5, description: 'in % oder px', value: '100%', type: PropertyTypes.string};
+        currentField.Properties.Height = {id: 6, description: 'in % oder px', value: '', type: PropertyTypes.string};
 
         currentTemplate.FieldValues[currentField.ID] = {};
         break;
