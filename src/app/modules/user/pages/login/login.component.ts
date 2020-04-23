61ebfb6b29998a8ffb1912c2df6af6db05315663
Luca <d3adly@outlook.de>
2020-04-23 14:35:36 +0200

    #724599

--- src/app/modules/user/pages/login/login.component.ts
+++ src/app/modules/user/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { FirebaseAuthService } from '../../services/firebase-auth.service';
 import { Router} from '@angular/router';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -21,31 +22,36 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  async userSubmittedForm() {
+  async userSubmittedForm(): Promise<boolean> {
     this.errorMessage = null;
+
+    if (this.loginForm.controls.email.invalid || this.loginForm.controls.password.invalid) {
+      this.errorMessage = 'Überprüfe bitte deine Eingaben, diese sind nicht valide.';
+      return true;
+    }
 
     const loginProcess = await this.fireService
       .login(this.loginForm.get('email').value, this.loginForm.get('password').value)
       .then(async value => {
-        await this.router.navigate(['/user']);
+        this.checkIfUserLoggedInAndRedirect();
       })
-      .catch(err => {
-        this.handleErrorOnCatch(err);
+      .catch((error: firebase.FirebaseError) => {
+        this.handleErrorOnCatch(error.message);
       });
+    return true;
   }
 
-  handleErrorOnCatch(err: string) {
-    console.log(err);
-    /*switch(err) {
-      case: '':
-        break;
-    }*/
-  }
-
-  async checkIfUserLoggedInAndRedirect() {
-    if (this.fireService.isLoggedIn) {
-     await this.router.navigate(['user']);
+  handleErrorOnCatch(err) {
+    this.errorMessage = 'Es ist ein Problem aufgetreten, versuche es bitte (später) erneut.';
+    if (err.includes('EMAIL_NOT_FOUND')) {
+      this.errorMessage = 'Diese E-Mail ist bei uns nicht hinterlegt, lege dir doch ein Konto an.';
     }
+  }
+  async checkIfUserLoggedInAndRedirect() {
+    if (await this.fireService.isLoggedIn) {
+     return true;
+    }
+    this.errorMessage = 'Leider trat ein Fehler auf und wir konnten dich nicht einloggen.';
   }
 
   isFieldValid(field) {
