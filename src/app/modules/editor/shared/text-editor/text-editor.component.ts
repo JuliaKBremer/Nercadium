31f8166eccb6ad116b283f266cfd596ef8744d5d
Timo Müller <timo.mueller92@outlook.de>
2020-06-08 09:14:14 +0200

    Fix Image upload in text editor.

--- src/app/modules/editor/shared/text-editor/text-editor.component.ts
+++ src/app/modules/editor/shared/text-editor/text-editor.component.ts
@@ -2,6 +2,7 @@ import {ViewEncapsulation, Component, Input} from '@angular/core';
 import {IBaseGameEntity} from '../../../../data/schema/Interfaces/Editor/IBaseGameEntity';
 
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Base64UploadAdapter from './Base64Upload';
 
 
 @Component({
@@ -16,6 +17,7 @@ export class TextEditorComponent {
   @Input() selectedObject: IBaseGameEntity;
 
   public Editor = ClassicEditor;
+  public editorConfig = { extraPlugins: [Base64UploadAdapter] };
 
   constructor() {
   }
