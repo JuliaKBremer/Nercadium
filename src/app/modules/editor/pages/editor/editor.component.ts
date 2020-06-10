7a8217de48567e4722d3540b6b141b47003caef9
Timo Müller <timo.mueller92@outlook.de>
2020-06-10 11:31:54 +0200

    Style Fields list.

--- src/app/modules/editor/pages/editor/editor.component.ts
+++ src/app/modules/editor/pages/editor/editor.component.ts
@@ -3,6 +3,7 @@ import {EditorService, RightSidebarStateEnum} from '../../services/editor/editor
 import {EntityTypeEnum} from '../../../../data/schema/Classes/Storage/EntityTypeEnum';
 import {IBaseGameEntity} from '../../../../data/schema/Interfaces/Editor/IBaseGameEntity';
 import {Subscription} from 'rxjs';
+import {FieldService} from '../../services/field/field.service';
 
 @Component({
   selector: 'app-editor',
@@ -19,7 +20,7 @@ export class EditorComponent implements OnInit, OnDestroy {
 
   private selectedObjectSubscription: Subscription;
 
-  constructor(public editorService: EditorService) { }
+  constructor(public editorService: EditorService, public fieldService: FieldService) { }
 
   ngOnInit() {
     this.selectedObjectSubscription = this.editorService.GetSelectedObjectAsObservable().subscribe(next => {
